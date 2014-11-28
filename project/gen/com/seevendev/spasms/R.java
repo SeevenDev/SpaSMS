3caa0bcc9fd53eef8f3b4e8ab445640e77a43ae5
SeevenDev <seeven.dev@gmail.com>
2014-11-28 20:08:39 +0100

    First working version
    - The autocomplete Recipient input only shows a list of the contacts
    numbers (without their names...)
    - You can write your message and choose how many times it should be
    sent.

--- project/gen/com/seevendev/spasms/R.java
+++ project/gen/com/seevendev/spasms/R.java
@@ -1091,7 +1091,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050040;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1110,8 +1110,10 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
+        public static final int iterations=0x7f05003f;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int message=0x7f05003d;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1119,6 +1121,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int recipient=0x7f05003c;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1129,6 +1132,7 @@ containing a value of this type.
         public static final int search_plate=0x7f050036;
         public static final int search_src_text=0x7f050037;
         public static final int search_voice_btn=0x7f05003b;
+        public static final int send=0x7f05003e;
         public static final int shortcut=0x7f05002e;
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
@@ -1239,9 +1243,12 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int iterations=0x7f0a0012;
+        public static final int message=0x7f0a0010;
+        public static final int recipient=0x7f0a000f;
+        public static final int send=0x7f0a0011;
     }
     public static final class style {
         /** 
--- project/src/com/seevendev/spasms/MainActivity.java
+++ project/src/com/seevendev/spasms/MainActivity.java
@@ -1,9 +1,18 @@
 package com.seevendev.spasms;
 
 import android.support.v7.app.ActionBarActivity;
+import android.telephony.SmsManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,6 +20,60 @@ public class MainActivity extends ActionBarActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// ==============================================================
+		// === VIEWS
+		// ==============================================================
+		
+		final AutoCompleteTextView recipientTextView = (AutoCompleteTextView) findViewById(R.id.recipient);
+		final EditText messageEditText = (EditText) findViewById(R.id.message);
+		final Button sendButton = (Button) findViewById(R.id.send);
+		final EditText iterationsEditText = (EditText) findViewById(R.id.iterations);
+		
+		// ==============================================================
+		// === CONTACTS AUTOCOMPLETE
+		// ==============================================================
+		
+		// Get a reference to the AutoCompleteTextView in the layout
+		AutoCompleteTextView recipient = (AutoCompleteTextView) findViewById(R.id.recipient);
+		
+		// Get the Contacts numbers :
+		Cursor contactCursor = getContentResolver().query(
+				ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+		contactCursor.moveToFirst();
+		
+		// Create the adapter and set it to the AutoCompleteTextView
+		ArrayAdapter<String> contactsAdapter = new ArrayAdapter<String>(
+				this, android.R.layout.simple_list_item_1 //, contactsList
+		);
+		
+		do {
+			String num = contactCursor.getString(contactCursor.getColumnIndex(CommonDataKinds.Phone.NUMBER));
+			contactsAdapter.add(num);
+		} while (contactCursor.moveToNext());
+		
+		recipient.setAdapter(contactsAdapter);
+		
+		// ==============================================================
+		// === SEND SMS
+		// ==============================================================
+		
+		final SmsManager smsManager = SmsManager.getDefault();
+		
+		sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Get Number, message and iterations :
+            	String num = recipientTextView.getText().toString();
+            	String message = messageEditText.getText().toString();
+            	int iterations = Integer.parseInt(iterationsEditText.getText().toString());
+            	
+            	// Send the SMS's :
+            	for (int i = 0 ; i < iterations ; i++) {
+            		smsManager.sendTextMessage(num, null, message, null, null);
+            	}
+            }
+        });
+		
 	}
 
 	@Override
